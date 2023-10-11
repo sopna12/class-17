80ba31d062573a4a1ead36bc8d761d1ba9f94cfc
sopna12 <sopna7077@gmail.com>
2023-10-11 11:13:51 +0600

    multi.js done

---
+++ class 17/multi.js
@@ -0,0 +1,13 @@
+const passage = `  Im sopna parvin.
+Im from Narayangonj.
+Im a big fan of srk.`
+// console.log(passage);
+
+
+
+const a = 10;
+const b = 20;
+
+// console.log(a, '+', b , '=', (a+b));
+
+console.log(`${a} + ${b} = ${a + b}`);
